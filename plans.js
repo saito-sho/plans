f3cd4f15a142770761629d187b76c57d05af1526
saito-sho <hyakuhati17@gmail.com>
2020-10-05 22:38:05 +0900

    変更

--- plans.js
+++ plans.js
@@ -1,7 +1,7 @@
 const params = (new URL(document.location)).searchParams;
 
 const company = params.get('company');
-const flight = params.get('flight')
+const flight = params.get('flight');
 
 window.document.getElementById('company').innerText = company;
 window.document.getElementById('flight').innerText = company;
