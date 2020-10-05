45bef7bd2959f0cabe9833182228ed1f838db9cc
saito-sho <hyakuhati17@gmail.com>
2020-10-05 19:48:22 +0900

    js変更

--- plans.js
+++ plans.js
@@ -1,8 +1,4 @@
-console.log("aaa");
-console.log(document.location);
 const params = (new URL(document.location)).searchParams;
-console.log(params)
+
 const company=params.get('company');
-console.log(params)
 window.document.getElementById('company').innerText = company;
-console.log(params)
