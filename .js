8422e034c33e0b8d53431f5d12228877e7ef7ac9
saito-sho <hyakuhati17@gmail.com>
2020-10-05 16:12:47 +0900

    js訂正

--- .js
+++ .js
@@ -1,4 +1,4 @@
 const params = (new URL(document.location)).searchParams;
 
-const company=params.get('company')
-window.document.getElementById('company').innerText = company
+const company=params.get('company');
+window.document.getElementById('company').innerText = company;
