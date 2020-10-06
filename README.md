670073cb3868c46dd6b91e4fd90304f7a87d56ff
saito-sho <hyakuhati17@gmail.com>
2020-10-06 20:37:20 +0900

    完成2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-https://saito-sho.github.io/plans/plans.html?company=ジョブキタ&flight=JK300&destination=エンジニア&time=15:00&changed_time=15:30&notice=さらなる準備のため
+https://saito-sho.github.io/plans/plans.html?company=ジョブキタ&flight=JK300&destination=エンジニア&time=15:00&changedTime=15:30&notice=さらなる準備のため
--- plans.js
+++ plans.js
@@ -5,15 +5,45 @@ const company = params.get('company');
 const flight = params.get('flight');
 const destination = params.get('destination');
 const time = params.get('time');
-const changed_time = params.get('changed_time');
+const changedTime = params.get('changedTime');
 const notice = params.get('notice');
 
-window.document.getElementById('company').innerText = company;
-window.document.getElementById('flight').innerText = flight;
-window.document.getElementById('destination').innerText = destination;
-window.document.getElementById('time').innerText = time;
-window.document.getElementById('changed_time').innerText = changed_time;
+// クエリパラメータチェック
+check('noneCompany', '会社名', 'company', company);
+check('noneFlight', '便名', 'flight', flight);
+check('noneDestination', '行先', 'destination', destination);
+checkTime();
 window.document.getElementById('notice').innerText = notice;
+description();
+
+function check(a, b, c, d) {
+    if (d === null || d === "") {
+        window.document.getElementById(a).innerText = `＊${b}を入力してください`;
+    } else {
+        window.document.getElementById(c).innerText = d;
+    }
+}
+
+function checkTime() {
+    if (time === null || time === "") {
+        window.document.getElementById('noneTime').innerText = '＊定刻を入力してください';
+    } else if (time !== null && changedTime === null || changedTime === "") {
+        window.document.getElementById('time').innerText = time;
+    } else {
+        window.document.getElementById('time').innerText = time;
+        window.document.getElementById('change').innerText = '変更';
+        window.document.getElementById('changedTime').innerText = '➡' + changedTime;
+    }
+}
+
+function description() {
+    if (company === null || flight === null || destination === null || time === null||company === "" || flight === "" || destination === "" || time === "") {
+        window.document.getElementById('description').innerText = 'クエリパラメータから入力してください';
+        window.document.getElementById('parapara').innerText = 'company=会社名　flight=便名　destination=行先　time=定刻　changedTime=定刻変更　notice=備考'
+        window.document.getElementById("error").style.border = "5px solid red";
+        window.document.getElementById("error").style.height = "90px";
+    }
+}
 
 // 今日の日付
 const today = new Date();
