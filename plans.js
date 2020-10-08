9ab51ff331dd0b3d3d201d6dd8f4eaba7c445a74
saito-sho <hyakuhati17@gmail.com>
2020-10-08 13:41:12 +0900

    完成4

--- plans.js
+++ plans.js
@@ -9,17 +9,21 @@ let time = params.get('time');
 let changedTime = params.get('changedTime');
 let notice = params.get('notice');
 
+
 // クエリパラメータチェック
-check('noneCompany', '会社名', 'company', company);
-check('noneFlight', '便名', 'flight', flight);
-check('noneDestination', '行先', 'destination', destination);
+check('noneCompany', '会社名', 'company', company, 12);
+check('noneFlight', '便名', 'flight', flight, 12);
+check('noneDestination', '行先', 'destination', destination, 10);
 checkTime();
-window.document.getElementById('notice').innerText = notice;
+checkNotice();
+checkQuery();
 description();
 
-function check(a, b, c, d) {
+function check(a, b, c, d, word) {
     if (d === null || d === "") {
         window.document.getElementById(a).innerText = `＊${b}を入力してください`;
+    } else if (d.length > word) {
+        window.document.getElementById(a).innerText = `＊${b}を${word}文字以内で入力してください`;
     } else {
         window.document.getElementById(c).innerText = d;
     }
@@ -47,74 +51,111 @@ function checkTime() {
     }
 }
 
-// エラー時の入力画面
-function description() {
-    if (company === null || flight === null || destination === null || time === null || company === "" || flight === "" || destination === "" || time === "" || !(time.match(/^[0-9:]+$/)) || !(changedTime.match(/^[0-9:]+$/))) {
+function checkNotice() {
+    if (notice === "" || notice === null) {
+        notice = "無し";
+    }
+    if (notice.length > 30) {
+        window.document.getElementById().innerText = '＊行先は30文字以内で入力してください';
+    } else {
+        window.document.getElementById('notice').innerText = notice;
+    }
+}
+
+
+    // エラー時の入力画面
+    function description() {
+        if (company === null || flight === null || destination === null || time === null || company === "" || flight === "" || destination === "" || time === "" || !(time.match(/^[0-9:]+$/)) || !(changedTime.match(/^[0-9:]+$/))
+            || company.length > 12 || flight.length > 12 || destination.length > 10 || notice.length > 30) {
+            window.document.getElementById('description').innerText = '必須項目を入力してください';
+            textCreate('textCompany', '会社名', '必須,12文字以内', company, 't-company', 12);
+            textCreate('textFlight', '便名', '必須,12文字以内', flight, 't-flight', 12);
+            textCreate('textDestination', '行先', '必須,10文字以内', destination, 't-destination', 10);
+            textCreate('textTime', '定刻', '必須,例：12:00', time, 't-time', 5);
+            textCreate('textChangedTime', '変更時間', '任意,例12:30', changedTime, 't-changedTime', 5);
+            textCreate('textNotice', '備考', '任意,30文字以内', notice, 't-notice', 30);
+            btnCreate1();
+            window.document.getElementById("error").style.border = "5px solid red";
+            window.document.getElementById("error").style.height = "160px";
+        } else {
+            btnCreate2()
+        }
+    }
+    // テキストボックスの表示
+    function textCreate(a, b, c, d, id, word) {
+        window.document.getElementById(a).innerText = b
+        const text = window.document.getElementById(a);
+        const input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("id", id)
+        input.setAttribute("maxlength", word);
+        input.setAttribute("placeholder", c);
+        if (time || changedTime) {
+            input.setAttribute("style", "”ime-mode:disabled;”")
+        }
+        text.appendChild(input);
+        if (d !== null || d !== "") {
+            window.document.getElementById(id).value = d
+
+        }
+    }
+
+    // 発行ボタンの表示
+    function btnCreate1() {
+        const text = window.document.getElementById('outputBtn');
+        const input = document.createElement("input");
+        input.setAttribute("type", "submit");
+        input.setAttribute("value", "決定");
+        input.setAttribute("onclick", "textInput()");
+        text.appendChild(input);
+    }
+
+    // パラメータ生成ボタン
+    function btnCreate2() {
+        const text = window.document.getElementById('parametersBtn');
+        const input = document.createElement("input");
+        input.setAttribute("type", "submit");
+        input.setAttribute("value", "データ更新");
+        input.setAttribute("onclick", "update()");
+        text.appendChild(input);
+    }
+
+    // 発行ボタン
+    function textInput() {
+        company = window.document.getElementById('t-company').value;
+        flight = window.document.getElementById('t-flight').value;
+        destination = window.document.getElementById('t-destination').value;
+        time = window.document.getElementById('t-time').value;
+        changedTime = window.document.getElementById('t-changedTime').value;
+        notice = window.document.getElementById('t-notice').value;
+        params.set('company', company);
+        params.set('flight', flight);
+        params.set('destination', destination);
+        params.set('time', time);
+        params.set('changedTime', changedTime);
+        params.set('notice', notice);
+
+        document.location = url + '?' + params.toString();
+    }
+    // パラメータの更新
+    function update() {
         window.document.getElementById('description').innerText = '必須項目を入力してください';
-        textCreate('textCompany', '会社名', '必須', company, 't-company');
-        textCreate('textFlight', '便名', '必須', flight, 't-flight');
-        textCreate('textDestination', '行先', '必須', destination, 't-destination');
-        textCreate('textTime', '定刻', '必須', time, 't-time');
-        textCreate('textChangedTime', '変更時間', "任意", changedTime, 't-changedTime');
-        textCreate('textNotice', '備考', '任意', notice, 't-notice');
-        btnCreate();
+        textCreate('textCompany', '会社名', '必須,12文字以内', company, 't-company', 12);
+        textCreate('textFlight', '便名', '必須,12文字以内', flight, 't-flight', 12);
+        textCreate('textDestination', '行先', '必須,10文字以内', destination, 't-destination', 10);
+        textCreate('textTime', '定刻', '必須,例:12:00', time, 't-time', 5);
+        textCreate('textChangedTime', '変更時間', '任意,例:12:30', changedTime, 't-changedTime', 5);
+        textCreate('textNotice', '備考', '任意,30文字以内', notice, 't-notice', 30);
+        btnCreate1();
         window.document.getElementById("error").style.border = "5px solid red";
         window.document.getElementById("error").style.height = "160px";
-    }
-}
-// テキストボックスの表示
-function textCreate(a, b, c, d, id) {
-    window.document.getElementById(a).innerText = b
-    const text = window.document.getElementById(a);
-    const input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("id", id)
-    input.setAttribute("maxlength", "5");
-    input.setAttribute("placeholder", c);
-    if (time || changedTime) {
-        input.setAttribute("style", "”ime-mode:disabled;”")
-    }
-    text.appendChild(input);
-    if (d !== null || d !== "") {
-        window.document.getElementById(id).value = d
 
     }
-}
-
-// ボタンの表示
-function btnCreate() {
-    const text = window.document.getElementById('outputBtn');
-    const input = document.createElement("input");
-    input.setAttribute("type", "submit");
-    input.setAttribute("value", "決定");
-    input.setAttribute("onclick", "textInput()");
-    text.appendChild(input);
-}
-
-// 発行ボタン
-function textInput() {
-    company = window.document.getElementById('t-company').value;
-    flight = window.document.getElementById('t-flight').value;
-    destination = window.document.getElementById('t-destination').value;
-    time = window.document.getElementById('t-time').value;
-    changedTime = window.document.getElementById('t-changedTime').value;
-    notice = window.document.getElementById('t-notice').value;
-    params.set('company', company);
-    params.set('flight', flight);
-    params.set('destination', destination);
-    params.set('time', time);
-    params.set('changedTime', changedTime);
-    params.set('notice', notice);
-    
-    document.location = url + '?' + params.toString();
-}
 
 
 
-
-
-// 今日の日付
-const today = new Date();
-const month = today.getMonth() + 1;
-const day = today.getDate();
-window.document.getElementById('today').innerText = month + "-" + day;
+    // 今日の日付
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+    window.document.getElementById('today').innerText = month + "-" + day;
